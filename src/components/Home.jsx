acd0a9bab43b7df9c4e141ed9607eb050a95afb1
Kevin V <kevinvoduy218@gmail.com>
2018-07-14 16:22:55 -0700

    [item] creating main profile layout

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import Slider from 'react-slick';
+import ItemMain from './ItemMain';
+import items from './items.json';
+
+console.log(items[0]);
+const settings = {
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+};
 
 const Home = () => (
-  <div>
-    home
+  <div className="home">
+    <Slider {...settings}>
+      <ItemMain item={items[0]} />
+    </Slider>
   </div>
 );
 
