0e5eacd7a57fac7419ca6298976ee4fe4860c732
Kevin V <kevinvoduy218@gmail.com>
2018-07-14 15:34:51 -0700

    [router]

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,58 @@
 import React from 'react';
 import './app.css';
+import Router from './Router';
 
 const App = () => (
-  <div className="nav">
-    <div className="logo">
-      <a href="/">
-        acronym
-      </a>
+  <div className="app">
+    <div className="nav">
+      <div className="logo">
+        <a href="/">
+          acronym
+        </a>
+      </div>
+
+      <div className="selector">
+        <ul id="select">
+
+          <li>
+            <a href="/">
+              img
+            </a>
+          </li>
+
+          <li>
+            <a href="/">
+              text
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div className="nav__links">
+        <ul id="links">
+
+          <li>
+            <a href="/">
+              Login
+            </a>
+          </li>
+
+          <li>
+            <a href="/">
+              Sign up
+            </a>
+          </li>
+
+          <li>
+            <a href="/">
+              Cart (0)
+            </a>
+          </li>
+
+        </ul>
+      </div>
     </div>
-
-    <div className="selector">
-      <ul id="select">
-
-        <li>
-          <a href="/">
-            img
-          </a>
-        </li>
-
-        <li>
-          <a href="/">
-            text
-          </a>
-        </li>
-      </ul>
-    </div>
-
-    <div className="nav__links">
-      <ul id="links">
-
-        <li>
-          <a href="/">
-            Login
-          </a>
-        </li>
-
-        <li>
-          <a href="/">
-            Sign up
-          </a>
-        </li>
-
-        <li>
-          <a href="/">
-            Cart (0)
-          </a>
-        </li>
-
-      </ul>
-    </div>
+    <Router />
   </div>
 );
 
